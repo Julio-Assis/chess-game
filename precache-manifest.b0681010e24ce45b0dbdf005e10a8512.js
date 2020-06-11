2265ee5ce75ae0dd0c340edf75930e458b4a18fa
Julio Assis <juliosevarollia@gmail.com>
2020-06-11 19:08:21 +0200

    Updates

---
+++ precache-manifest.b0681010e24ce45b0dbdf005e10a8512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3405a8872b31ad22af1832521741c6",
+    "url": "/chess-game/index.html"
+  },
+  {
+    "revision": "ea54f4a647ea2e79983a",
+    "url": "/chess-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f79817f9680d7b8fe295",
+    "url": "/chess-game/static/js/2.936a60b4.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/chess-game/static/js/2.936a60b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea54f4a647ea2e79983a",
+    "url": "/chess-game/static/js/main.e9d2f397.chunk.js"
+  },
+  {
+    "revision": "9577bf7f288c08df764c",
+    "url": "/chess-game/static/js/runtime-main.ad28f217.js"
+  }
+]);
